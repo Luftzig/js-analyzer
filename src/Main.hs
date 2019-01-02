c70ab6f93ada69fd6fc8aa7ba4dda4053a455cda
Yoav Luft <yoav.luft@prototyp.se>
2019-01-02 20:53:27 +0100

    Merge out/projects.json with old one before writing

--- src/Main.hs
+++ src/Main.hs
@@ -2,52 +2,56 @@
 
 module Main where
 
-import qualified GitHub.Auth as Auth
-import qualified GitHub.Data as Github
-import qualified GitHub.Data.Name as Github
-import qualified GitHub.Endpoints.Search as Github
-import qualified GitHub.Endpoints.Repos as Github
-import qualified GitHub.Request as Github
+import qualified GitHub.Auth              as Auth
+import qualified GitHub.Data              as Github
+import qualified GitHub.Data.Name         as Github
+import qualified GitHub.Endpoints.Repos   as Github
+import qualified GitHub.Endpoints.Search  as Github
+import qualified GitHub.Request           as Github
 
-import Control.Monad (forM_)
-import Control.Monad.IO.Class (liftIO)
-import qualified Data.Aeson as A
-import qualified Data.Aeson.Text as A
+import           Control.Monad            (forM_)
+import           Control.Monad.IO.Class   (liftIO)
+import qualified Data.Aeson               as A
 import qualified Data.Aeson.Encode.Pretty as A
-import Data.List (intercalate)
-import Data.ByteString.Char8 (ByteString, pack)
-import qualified Data.ByteString.Lazy as LBS
-import Data.Either (either, lefts, rights, fromRight, isLeft)
-import Data.Either.Combinators (mapLeft)
-import Data.Maybe (listToMaybe, fromMaybe, isNothing)
-import Data.String (fromString)
-import Data.Text (Text, breakOn, intercalate, unpack, lines, splitOn, strip, tail)
-import qualified Data.Text.IO as T
-import Data.Vector (Vector, fromList)
-import qualified Data.Text.Lazy.IO as TextIO
-import Data.Semigroup ((<>))
-import qualified Data.Text.Encoding as TE
-import Network.URI (URI, uriToString)
-import System.Directory (createDirectoryIfMissing)
-import System.Environment (lookupEnv, getArgs)
-import System.FilePath.Posix ((</>))
-import System.IO (FilePath)
-import Options.Applicative
+import qualified Data.Aeson.Text          as A
+import           Data.ByteString.Char8    (ByteString, pack)
+import qualified Data.ByteString.Lazy     as LBS
+import           Data.Either              (either, fromRight, isLeft, lefts,
+                                           rights)
+import           Data.Either.Combinators  (mapLeft)
+import qualified Data.HashMap.Strict      as HM
+import           Data.List                (intercalate)
+import           Data.Maybe               (fromMaybe, isNothing, listToMaybe)
+import           Data.Semigroup           ((<>))
+import           Data.String              (fromString)
+import           Data.Text                (Text, breakOn, intercalate, lines,
+                                           splitOn, strip, tail, unpack)
+import qualified Data.Text.Encoding       as TE
+import qualified Data.Text.IO             as T
+import qualified Data.Text.Lazy.IO        as TextIO
+import           Data.Vector              (Vector, fromList)
+import           Network.URI              (URI, uriToString)
+import           Options.Applicative
+import           System.Directory         (createDirectoryIfMissing)
+import           System.Environment       (getArgs, lookupEnv)
+import           System.FilePath.Posix    ((</>))
+import           System.IO                (FilePath)
 
-import AnalyzeContent (analyze)
-import Projects (toProjectInfo)
-import Data
+import           AnalyzeContent           (analyze)
+import           Data
+import           Projects                 (toProjectInfo)
 
 
 data Options = Options
-    { searchQuery :: String
-    , page :: Int
-    , numResults :: Int
-    , sort :: String
-    , order :: String
-    , outputDir :: String
-    , reposFile :: Maybe String
-    , process :: Bool
+    { searchQuery   :: String
+    , page          :: Int
+    , numResults    :: Int
+    , sort          :: String
+    , order         :: String
+    , outputDir     :: String
+    , reposFile     :: Maybe String
+    , process       :: Bool
+    , noUpdateRepos :: Bool
     }
 
 
@@ -64,9 +68,9 @@ main = do
     createDirectoryIfMissing True $ outputDir args
     result <- getReposToProcess args auth
     case (process args, result) of
-      (_, Left e) -> putStrLn $ "Error: " ++ show e
-      (True, Right r) -> analyzeRepos auth (outputDir args) r
-      (False, Right rs) -> putStrLn $ show $ fmap repoId rs
+      (_, Left e)       -> putStrLn $ "Error: " ++ show e
+      (True, Right r)   -> analyzeRepos auth (outputDir args) r
+      (False, Right rs) -> print $ fmap repoId rs
     where
       repoId r = Data.Text.intercalate "/" [Github.untagName $ Github.simpleOwnerLogin $ Github.repoOwner r, Github.untagName $ Github.repoName r]
 
@@ -126,6 +130,11 @@ args = Options
     <> help "if set, will run the processing chain on the input (file or search) repositories. Otherwise will just print repositories metadata"
     <> showDefault
     )
+  <*> switch
+    ( long "no-update-projects"
+    <> short 'U'
+    <> help "If `OUTPUT_DIR/projects.json` exists, do not update or fetch projects that are already listed"
+    )
 
 
 reposFileOption :: Parser (Maybe FilePath)
@@ -137,7 +146,7 @@ reposFileOption = Just <$> strOption
 
 
 searchReposOption :: Parser (Maybe FilePath)
-searchReposOption= const Nothing <$> switch
+searchReposOption= Nothing <$ switch
   ( long "search"
   <> short 'S'
   <> help "Search github for repositories to process"
@@ -149,7 +158,7 @@ repos = reposFileOption <|> searchReposOption
 
 
 getReposToProcess :: Options -> Maybe Github.Auth -> IO (Either Error (Vector Github.Repo))
-getReposToProcess args@(Options {reposFile=Nothing}) auth = do
+getReposToProcess args@Options {reposFile=Nothing} auth = do
     let search = fromString $ searchQuery args
     putStrLn $ "Searching " ++ unpack search
     result <- searchRepos auth search
@@ -161,17 +170,17 @@ getReposToProcess args@(Options {reposFile=Nothing}) auth = do
     return $ mapLeft GithubError result
 
 
-getReposToProcess args@(Options {reposFile=(Just file)}) auth = do
-    resolveRepositoriesFromList auth file
+getReposToProcess args@Options {reposFile=(Just file), noUpdateRepos=noUpdate} auth =
+    resolveRepositoriesFromList auth args
 
 
-resolveRepositoriesFromList :: Maybe Github.Auth -> FilePath -> IO (Either Error (Vector Github.Repo))
-resolveRepositoriesFromList auth file = do
+resolveRepositoriesFromList :: Maybe Github.Auth -> Options -> IO (Either Error (Vector Github.Repo))
+resolveRepositoriesFromList auth Options {reposFile=(Just file), noUpdateRepos=noUpdate, outputDir=outDir} = do
   fileContent <- T.readFile file
-  names <- return $ concatMap (splitOn ",") $ Data.Text.lines fileContent
+  let names = concatMap (splitOn ",") $ Data.Text.lines fileContent
   repos <- mapM (getRepoInfo auth) names
-  failures <- return $ lefts repos
-  successes <- return $ rights repos
+  let failures = lefts repos
+  let successes = rights repos
   case failures of
     []   -> return $ Right $ fromList successes
     f:fs -> return $ Left $ GithubError f
@@ -180,7 +189,7 @@ resolveRepositoriesFromList auth file = do
 getRepoInfo :: Maybe Github.Auth -> Text -> IO (Either Github.Error Github.Repo)
 getRepoInfo auth fullName = do
   let (ownerT, repoT) = Data.Text.breakOn "/" fullName
-  let (owner, repo) = (Github.N $ Data.Text.strip $ ownerT, Github.N $ Data.Text.strip $ Data.Text.tail repoT)
+  let (owner, repo) = (Github.N $ Data.Text.strip ownerT, Github.N $ Data.Text.strip $ Data.Text.tail repoT)
   Github.repository' auth owner repo
 
 
@@ -200,7 +209,18 @@ analyzeRepos auth outputDir result = do
 
 writeProjectsData :: FilePath -> Vector ProjectInfo -> IO ()
 writeProjectsData outputDir projects = do
-  let projectsFile = outputDir </> "projects.json"
-  oldProjects <- A.eitherDecodeFileStrict projectsFile :: IO (Either String (Vector ProjectInfo))
-  either (\err -> print ("Failed to read old projects: " ++ err)) (\_ -> mempty) oldProjects
-  LBS.writeFile projectsFile (A.encodePretty $ (projects <> fromRight mempty oldProjects))
+    let projectsFile = outputDir </> "projects.json"
+    oldProjects <- getExistingProjectsList outputDir
+    let projectsMap = foldl (\m p -> HM.insert (projectId p) p m) HM.empty projects
+    let allProjects = HM.union projectsMap oldProjects
+    LBS.writeFile projectsFile (A.encodePretty (HM.elems allProjects))
+
+
+getExistingProjectsList :: FilePath -> IO (HM.HashMap Text ProjectInfo)
+getExistingProjectsList outDir = do
+  let path = outDir </> "projects.json"
+  oldProjects <- A.decodeFileStrict path :: IO (Maybe (Vector ProjectInfo))
+  return $ case oldProjects of
+    Just ps -> foldl (\m p -> HM.insert (projectId p) p m) HM.empty ps
+    Nothing -> HM.empty
+
