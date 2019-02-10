96f4e064cbd6d1a7270a6b3f5fb735d24e32c33c
Yoav Luft <yoav.luft@prototyp.se>
2019-02-10 19:43:19 +0100

    removing accidently added files

--- talk/.bundle/gems/ruby/2.3.0/gems/asciidoctor-diagram-1.5.13/lib/asciidoctor-diagram/tikz.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'extensions'
-
-Asciidoctor::Extensions.register do
-  require_relative 'tikz/extension'
-  block Asciidoctor::Diagram::TikZBlockProcessor, :tikz
-  block_macro Asciidoctor::Diagram::TikZBlockMacroProcessor, :tikz
-end
--- talk/.bundle/gems/ruby/2.3.0/gems/asciidoctor-diagram-1.5.13/lib/asciidoctor-diagram/version.rb
+++
@@ -1,5 +0,0 @@
-module Asciidoctor
-  module Diagram
-    VERSION = "1.5.13"
-  end
-end
